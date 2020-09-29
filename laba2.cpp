42ca80bcbcd5582f266f94b0a33df03827184fdf
privetyaonton <71338577+privetyaonton@users.noreply.github.com>
2020-09-29 21:09:38 +0300

    Add files via upload

---
+++ laba2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, b, h, x;
+	double y, y1, y2;
+	scanf_s("%d%d%d", &a, &b, &h);
+	x = a * b * h;
+	y1 = pow(x, 2) + sin(x) + 3 * sqrt(cos(2 * x));
+	y2 = (3 * log(x)) / 2;
+	y = y1 / y2;
+	printf_s("\n%f", y);
+	return 0;
+}
