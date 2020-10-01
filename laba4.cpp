f598b1a5bc86d3b1d8ff1971ca9b506cad73034a
privetyaonton <71338577+privetyaonton@users.noreply.github.com>
2020-10-01 19:30:32 +0300

    Add files via upload

---
+++ laba4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+int main()
+{
+	int i,c;
+	int a[15];
+	for (i = 0; i < 15; i++)
+		scanf_s("%d",  &a[i]);
+	for (i = 0; i < 7; i++)
+	{
+		c = a[i];
+		a[i] = a[14 - i];
+		a[14 - i] = c;
+	}
+	for (i = 0; i < 15; i++)
+		printf("%d ", a[i]);
+	return 0;
+}
