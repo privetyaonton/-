4c117e8935b8026a33c005ec7e7a04fc82817c78
privetyaonton <71338577+privetyaonton@users.noreply.github.com>
2021-01-09 01:07:17 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const int n = 100;
+    char* symbols = (char*)malloc(sizeof(char) * n);
+    printf("Введите символы (%d):\n", n);
+    fgets(symbols, n, stdin);
+    int i = 0;
+    while (symbols[i] != ':') {
+        i++;
+    }
+    i++;
+    while (symbols[i] != ':') {
+        if (symbols[i] == '\0') break;
+        putchar(symbols[i]);
+        i++;
+    }
+}
